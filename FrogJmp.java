0ea5b2f284ede15161a5ab175cb23585908eae2d
amriksingh <amrikamrican@gmail.com>
2015-02-17 05:33:54 -0500

    not a 100% correct solution

---
+++ FrogJmp.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int X, int Y, int D) {
+        // write your code in Java SE 8
+        int diff = Y - X;
+        int steps = 0;
+        if( D != 0){
+            if((diff % D) == 0)
+                steps = diff %  D;
+            else
+                steps = diff / D + 1;
+        }
+        return steps;
+    }
+}
