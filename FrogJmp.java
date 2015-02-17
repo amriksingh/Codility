1037a9971c8441c067ce6b22873b562ec73c7f66
amriksingh <amrikamrican@gmail.com>
2015-02-17 08:08:42 -0500

    No need to check if D == 0

--- FrogJmp.java
+++ FrogJmp.java
@@ -3,12 +3,11 @@ class Solution {
         // write your code in Java SE 8
         int diff = Y - X;
         int steps = 0;
-        if( D != 0){
-            if((diff % D) == 0)
-                steps = diff /  D;
-            else
-                steps = diff / D + 1;
-        }
+		if((diff % D) == 0)
+			steps = diff /  D;
+		else
+			steps = diff / D + 1;
+			
         return steps;
     }
 }
