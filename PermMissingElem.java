f068868266d49b60f462484ea424ccfdd6862b68
amriksingh <amrikamrican@gmail.com>
2015-02-17 08:22:23 -0500

    better solution

--- PermMissingElem.java
+++ PermMissingElem.java
@@ -2,18 +2,27 @@ class Solution {
     public int solution(int[] A) {
         // write your code in Java SE 8
         int len = A.length;
+        int max = 0;
+        int counter = 0;
         if(len == 0)
             return 0;
-        int[] count = new int[A.length+1];
+        int[] count = new int[len+1];
         int missing = 0;
-        for(int i = 0; i < A.length; i++){
+        for(int i = 0; i < len; i++){
+            if(max < A[i]){
+                max = A[i];
+            }
             count[A[i]-1] = 1;
         }
-        for(int i = 0; i < A.length; i++){
-            if(count[i] == 0)
+        for(int i = 0; i < len; i++){
+            if(count[i] == 1){
+                counter += 1;
+                if( counter == max & count[i] == 1)
+                    missing = max+1;
+            }
+            else
                 missing = i+1;
         }
         return missing;
     }
 }
-
