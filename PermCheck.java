f122fbad85f9db337f7a8009f1dbffe63329e256
amriksingh <amrikamrican@gmail.com>
2015-02-24 12:53:11 -0500

    checking out of bounds
    and some comments

--- PermCheck.java
+++ PermCheck.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(int[] A) {
-        int len = A.length;
-        //System.out.print(len);
-        int[] count = new int[len+1];
+        int len = A.length;					// Length of array
+        int[] count = new int[len+1];		//new array for counting
         for(int i = 0; i < len; i++){
-           // System.out.println(A[i]);
-            count[A[i]-1] += 1;  
-           // System.out.println(i + " " + count[i]);
+            if(A[i] > len+1 || A[i] == 0){	//out of bounds
+                return 0;
+			}
+            count[A[i]-1] += 1;				// lets keep track  
         } 
         for(int i = 0; i < len; i++){
-            //System.out.println(i);
-            if(count[i] == 0)
-                return 0;
+            if(count[i] == 0){				// if any place missing
+                return 0;					// not a permutation
+			}
         }
         return 1;
     }
