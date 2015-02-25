d90a2184f35cf4e18c579c5d82772c22c702f45f
amriksingh <amrikamrican@gmail.com>
2015-02-24 21:30:19 -0500

    18

--- FrogRiverOne.java
+++ FrogRiverOne.java
@@ -1,26 +1,22 @@
 class Solution {
     public int solution(int X, int[] A) {
         int pos = 0;
-        Boolean found = false;
         int[] count = new int[A.length+1];
         for(int i = 0; i < A.length; i++){
             if(A[i] == X){
                 pos = i;
             }
-            if(i < X && A[i] == X){
-                return -1;
-            }
-            else{
-                count[A[i]] += 1;
+            count[A[i]] += 1;
+			if(i < X && A[i] == X){
+                count[A[i]] = 0;
             }
         }
         for(int i = 1; i <= X; i++){
             if(count[i] >=1){
-                found = true;
             }else{
                 return -1;
             }
-        }
+        }			
         return pos;
     }
 }
