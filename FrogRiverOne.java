1361d57ca25c81c86a12e562de2bccd7674e3832
amriksingh <amrikamrican@gmail.com>
2015-02-26 16:00:28 -0500

    still not perfect

--- FrogRiverOne.java
+++ FrogRiverOne.java
@@ -3,11 +3,13 @@ class Solution {
         int jmp = 0;
         int[] count = new int[A.length+1];
         for(int i = 0; i < A.length; i++){
-            if(count[A[i]] == 0){
-                count[A[i]] = A[i];
-            jmp++;
-            if(jmp == X)
-                return i;
+            if(A[i] <= X && A[i] < A.length){
+                    if(count[A[i]] == 0){
+                        count[A[i]] = A[i];
+                        jmp++;
+                    if(jmp == X)
+                        return i;
+                }
             }
         }
         return -1;
